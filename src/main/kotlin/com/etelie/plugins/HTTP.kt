f28eb01205c23bad16980e76d55228ba633aaae9
Zachary Siegel <zachary@etelie.com>
2023-03-11 15:01:39 -0600

    remove https redirect + hsts (#2)

--- src/main/kotlin/com/etelie/plugins/HTTP.kt
+++ src/main/kotlin/com/etelie/plugins/HTTP.kt
@@ -5,19 +5,17 @@ import io.ktor.http.content.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.cachingheaders.*
 import io.ktor.server.plugins.defaultheaders.*
-import io.ktor.server.plugins.hsts.*
-import io.ktor.server.plugins.httpsredirect.*
 
 fun Application.configureHTTP() {
-    install(HttpsRedirect) {
-            // The port to redirect to. By default 443, the default HTTPS port.
-            sslPort = 443
-            // 301 Moved Permanently, or 302 Found redirect.
-            permanentRedirect = true
-        }
-    install(HSTS) {
-        includeSubDomains = true
-    }
+//    install(HttpsRedirect) {
+//            // The port to redirect to. By default 443, the default HTTPS port.
+//            sslPort = 443
+//            // 301 Moved Permanently, or 302 Found redirect.
+//            permanentRedirect = true
+//        }
+//    install(HSTS) {
+//        includeSubDomains = true
+//    }
     install(DefaultHeaders) {
         header("X-Engine", "Ktor") // will send this header with each response
     }
