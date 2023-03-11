704d1192b7842faf7fcb34cb60f21595783ac4a2
Zachary Siegel <zachary@etelie.com>
2023-03-11 15:00:39 -0600

    remove https redirect + hsts

--- src/main/kotlin/com/etelie/plugins/Monitoring.kt
+++ src/main/kotlin/com/etelie/plugins/Monitoring.kt
@@ -1,33 +1,23 @@
 package com.etelie.plugins
 
-import io.micrometer.prometheus.*
-import io.ktor.server.metrics.micrometer.*
-import io.ktor.server.response.*
-import io.ktor.server.metrics.dropwizard.*
-import com.codahale.metrics.*
-import io.ktor.server.plugins.callloging.*
-import org.slf4j.event.*
-import io.ktor.server.request.*
 import io.ktor.http.*
-import io.ktor.server.plugins.callid.*
 import io.ktor.server.application.*
+import io.ktor.server.metrics.micrometer.*
+import io.ktor.server.plugins.callid.*
+import io.ktor.server.plugins.callloging.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import java.util.concurrent.TimeUnit
+import io.micrometer.prometheus.PrometheusConfig
+import io.micrometer.prometheus.PrometheusMeterRegistry
+import org.slf4j.event.Level
 
 fun Application.configureMonitoring() {
     val appMicrometerRegistry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
     
-        install(MicrometerMetrics) {
-            registry = appMicrometerRegistry
-            // ...
-        }
-    install(DropwizardMetrics) {
-      Slf4jReporter.forRegistry(registry)
-        .outputTo(this@configureMonitoring.log)
-        .convertRatesTo(TimeUnit.SECONDS)
-        .convertDurationsTo(TimeUnit.MILLISECONDS)
-        .build()
-        .start(10, TimeUnit.SECONDS)
+    install(MicrometerMetrics) {
+        registry = appMicrometerRegistry
+        // ...
     }
     install(CallLogging) {
         level = Level.INFO
