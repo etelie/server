05a10c330f1144fe724877db9edcafcc2a174786
Zachary Siegel <zachary@etelie.com>
2023-03-22 00:55:43 -0500

    docker push build to ecr (#9)
    * build tag + variable renaming
    
    * fix renamed properties
    
    * repo host qa/prod + dont use buildEnv in repo name
    
    * server doesnt know about repo host
    
    * PROJECT_VERSION -> BUILD_TAG
    
    * update readme: avlo

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,17 @@
 import org.jetbrains.kotlin.utils.addToStdlib.ifTrue
 
-val org_id: String by project
-val module_id: String by project
-val repository_host: String by project
-val build_number: String = System.getenv("BUILD_NUMBER").orEmpty().ifEmpty { "dev" }
-val build_env: String = System.getenv("BUILD_ENV").orEmpty().ifEmpty { "dev" }
+val orgId: String by project
+val moduleId: String by project
+val versionNumber: String = System.getenv("VERSION_NUMBER").orEmpty().ifEmpty { "0.0.0" }
+val buildTag: String = System.getenv("BUILD_TAG").orEmpty().ifEmpty { "0.0.0-dev" }
 val isDevelopment: Boolean = project.ext.has("development")
 
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-val postgres_version: String by project
-val h2_version: String by project
-val prometeus_version: String by project
+val ktorVersion: String by project
+val kotlinVersion: String by project
+val logbackVersion: String by project
+val postgresVersion: String by project
+val h2Version: String by project
+val prometeusVersion: String by project
 
 plugins {
     kotlin("jvm") version "1.8.10"
@@ -21,11 +20,9 @@ plugins {
     id("com.palantir.docker") version "0.34.0"
 }
 
-group = "com.etelie"
-version = "0.0.1-${build_number}"
+group = "com.${orgId}" // "com.etelie"
+version = buildTag     // "{version}-{build}"
 application {
-    assert(isDevelopment && build_env == "dev")
-
     mainClass.set("com.etelie.ApplicationKt")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
@@ -35,27 +32,27 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
-    implementation("org.postgresql:postgresql:$postgres_version")
-    implementation("com.h2database:h2:$h2_version")
-    implementation("io.ktor:ktor-server-metrics-micrometer-jvm:$ktor_version")
-    implementation("io.micrometer:micrometer-registry-prometheus:$prometeus_version")
-    implementation("io.ktor:ktor-server-metrics-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-call-logging-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-call-id-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-http-redirect-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-hsts-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-default-headers-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-caching-headers-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-resources:$ktor_version")
-    implementation("io.ktor:ktor-server-auto-head-response-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-auth-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
+    implementation("org.postgresql:postgresql:$postgresVersion")
+    implementation("com.h2database:h2:$h2Version")
+    implementation("io.ktor:ktor-server-metrics-micrometer-jvm:$ktorVersion")
+    implementation("io.micrometer:micrometer-registry-prometheus:$prometeusVersion")
+    implementation("io.ktor:ktor-server-metrics-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-call-logging-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-call-id-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-http-redirect-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-hsts-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-default-headers-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-caching-headers-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-resources:$ktorVersion")
+    implementation("io.ktor:ktor-server-auto-head-response-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-auth-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
 }
 
 docker {
@@ -63,9 +60,7 @@ docker {
         throw Exception("Project version not found")
     }
 
-    val repository: String = if (isDevelopment)
-        "${org_id}/${build_env}/${module_id}" else
-        "${repository_host}/${org_id}/${build_env}/${module_id}"
+    val repository: String = "${orgId}/${moduleId}"
     val tag: String = project.version.toString()
 
     name = "${repository}:${tag}"
@@ -73,7 +68,7 @@ docker {
     files(fileTree("./build/libs/"))
     buildArgs(
         mapOf(
-            "PROJECT_VERSION" to project.version.toString()
+            "BUILD_TAG" to buildTag
         )
     )
     noCache(true)
