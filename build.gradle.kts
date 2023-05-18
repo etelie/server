1caf9cf8c611bac136fe399b7d02c624e5ef7443
Zachary Siegel <zachary@etelie.com>
2023-05-17 22:00:57 -0500

    minor

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,62 +14,67 @@ val h2Version: String by project
 val prometeusVersion: String by project
 
 plugins {
-    kotlin("jvm") version "1.8.10"
-    id("io.ktor.plugin") version "2.2.4"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
-    id("com.palantir.docker") version "0.34.0"
+  kotlin("jvm") version "1.8.10"
+  id("io.ktor.plugin") version "2.2.4"
+  id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
+  id("com.palantir.docker") version "0.34.0"
 }
 
 group = "com.${orgId}" // "com.etelie"
 version = buildTag     // "{version}-{build}"
 application {
-    mainClass.set("com.etelie.ApplicationKt")
-    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+  mainClass.set("com.etelie.ApplicationKt")
+  applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
 repositories {
-    mavenCentral()
+  mavenCentral()
+  google()
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
-    implementation("org.postgresql:postgresql:$postgresVersion")
-    implementation("com.h2database:h2:$h2Version")
-    implementation("io.ktor:ktor-server-metrics-micrometer-jvm:$ktorVersion")
-    implementation("io.micrometer:micrometer-registry-prometheus:$prometeusVersion")
-    implementation("io.ktor:ktor-server-metrics-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-call-logging-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-call-id-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-http-redirect-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-hsts-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-default-headers-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-caching-headers-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-resources:$ktorVersion")
-    implementation("io.ktor:ktor-server-auto-head-response-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-auth-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
-    implementation("ch.qos.logback:logback-classic:$logbackVersion")
-    testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+  implementation("org.postgresql:postgresql:$postgresVersion")
+  implementation("com.h2database:h2:$h2Version")
+  implementation("io.micrometer:micrometer-registry-prometheus:$prometeusVersion")
+  implementation("ch.qos.logback:logback-classic:$logbackVersion")
+
+  // Ktor
+  implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
+  implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktorVersion")
+  implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
+  implementation("io.ktor:ktor-server-metrics-micrometer-jvm:$ktorVersion")
+  implementation("io.ktor:ktor-server-metrics-jvm:$ktorVersion")
+  implementation("io.ktor:ktor-server-call-logging-jvm:$ktorVersion")
+  implementation("io.ktor:ktor-server-call-id-jvm:$ktorVersion")
+  implementation("io.ktor:ktor-server-http-redirect-jvm:$ktorVersion")
+  implementation("io.ktor:ktor-server-hsts-jvm:$ktorVersion")
+  implementation("io.ktor:ktor-server-default-headers-jvm:$ktorVersion")
+  implementation("io.ktor:ktor-server-caching-headers-jvm:$ktorVersion")
+  implementation("io.ktor:ktor-server-resources:$ktorVersion")
+  implementation("io.ktor:ktor-server-auto-head-response-jvm:$ktorVersion")
+  implementation("io.ktor:ktor-server-auth-jvm:$ktorVersion")
+  implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
+  implementation("io.ktor:ktor-server-cors:$ktorVersion")
+
+  testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+  testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
 }
 
 docker {
-    project.version.toString().isEmpty().ifTrue {
-        throw Exception("Project version not found")
-    }
+  project.version.toString().isEmpty().ifTrue {
+    throw Exception("Project version not found")
+  }
 
-    val repository: String = "${orgId}/${moduleId}"
-    val tag: String = project.version.toString()
+  val repository: String = "${orgId}/${moduleId}"
+  val tag: String = project.version.toString()
 
-    name = "${repository}:${tag}"
-    setDockerfile(File("./docker/deploy/Dockerfile"))
-    files(fileTree("./build/libs/"))
-    buildArgs(
-        mapOf(
-            "BUILD_TAG" to buildTag
-        )
+  name = "${repository}:${tag}"
+  setDockerfile(File("./docker/deploy/Dockerfile"))
+  files(fileTree("./build/libs/"))
+  buildArgs(
+    mapOf(
+      "BUILD_TAG" to buildTag
     )
-    noCache(true)
+  )
+  noCache(true)
 }
