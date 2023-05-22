1d2babb8726dda437624ef4efad8cc54bef4e995
Zachary Siegel <zachary@etelie.com>
2023-05-22 13:26:56 -0500

    test setup

--- src/test/kotlin/com/etelie/ApplicationTest.kt
+++ src/test/kotlin/com/etelie/ApplicationTest.kt
@@ -1,21 +1,43 @@
 package com.etelie
 
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import io.ktor.server.testing.*
-import kotlin.test.*
-import io.ktor.http.*
-import com.etelie.plugin.*
+import com.etelie.plugin.pluginApi
+import com.etelie.plugin.pluginHTTP
+import com.etelie.plugin.pluginMonitoring
+import com.etelie.plugin.pluginRouting
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.Application
+import io.ktor.server.testing.testApplication
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
+
+    private fun Application.setup() {
+        pluginApi()
+        pluginHTTP()
+        pluginMonitoring()
+        pluginRouting()
+    }
+
     @Test
-    fun testRoot() = testApplication {
-        application {
-            pluginApi()
-        }
+    fun root() = testApplication {
+        application { setup() }
         client.get("/").apply {
             assertEquals(HttpStatusCode.OK, status)
-            assertEquals("Hello World!", bodyAsText())
+            assertEquals("etelie api", bodyAsText())
         }
     }
+
+    @Test
+    fun healthcheck() = testApplication {
+        application { setup() }
+        client.get("/healthcheck").also { response ->
+            assertEquals(HttpStatusCode.OK, response.status)
+            assertEquals("Service is healthy", response.body())
+        }
+    }
+
 }
