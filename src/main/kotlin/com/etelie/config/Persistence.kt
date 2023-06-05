fbcad3d0f21244cc2f5b2b129c67165c039ce55a
Zachary Siegel <zachary@etelie.com>
2023-06-04 20:23:20 -0500

    hikaricp setup

--- src/main/kotlin/com/etelie/config/Persistence.kt
+++ src/main/kotlin/com/etelie/config/Persistence.kt
@@ -1,5 +1,7 @@
 package com.etelie.config
 
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
 import io.ktor.server.application.Application
 import kotlinx.coroutines.Dispatchers
 import org.jetbrains.exposed.sql.Database
@@ -9,6 +11,7 @@ import org.jetbrains.exposed.sql.exposedLogger
 import org.jetbrains.exposed.sql.transactions.TransactionManager
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.sql.Connection
 
 object Persistence {
 
@@ -24,11 +27,13 @@ object Persistence {
         val user = environment.config.property("postgresql.credential.user").getString()
         val password = environment.config.property("postgresql.credential.password").getString()
         val db = environment.config.property("postgresql.database").getString()
+        val maxConnections = environment.config.property("hikaricp.max_connections").getString().toInt()
 
         val jdbcUrl = "jdbc:postgresql://$host:$port/$db"
-        val database = Database.connect(jdbcUrl, driverClassName, user, password)
-        TransactionManager.defaultDatabase = database
+        val dataSource = createHikariDataSource(jdbcUrl, maxConnections, user, password)
+        val database = Database.connect(dataSource)
 
+        TransactionManager.defaultDatabase = database
         exposedLogger.info("Successfully connected to ${database.url}")
     }
 
@@ -45,5 +50,23 @@ object Persistence {
             return@newSuspendedTransaction block()
         }
     }
+
+    private fun createHikariDataSource(
+        jdbcUrl: String,
+        maxConnections: Int,
+        user: String,
+        password: String,
+    ) = HikariDataSource(
+        HikariConfig().also {
+            it.driverClassName = Persistence.driverClassName
+            it.jdbcUrl = jdbcUrl
+            it.maximumPoolSize = maxConnections
+            it.isAutoCommit = false
+            it.transactionIsolation = Connection::TRANSACTION_REPEATABLE_READ.name
+            it.username = user
+            it.password = password
+            it.validate()
+        },
+    )
 
 }
