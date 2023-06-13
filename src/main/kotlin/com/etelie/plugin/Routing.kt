e9c8f0a367d7f2a78b35eec5efd2b125a3b1956a
Zachary Siegel <zachary@etelie.com>
2023-06-13 00:43:11 -0500

    remove root route

--- src/main/kotlin/com/etelie/plugin/Routing.kt
+++ src/main/kotlin/com/etelie/plugin/Routing.kt
@@ -1,22 +1,11 @@
 package com.etelie.plugin
 
 import com.etelie.control.controlRoutes
-import guru.zoroark.tegral.openapi.ktor.describe
-import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
-import io.ktor.server.application.call
-import io.ktor.server.response.respond
-import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
 
 fun Application.pluginRouting() {
     routing {
-        get("/") {
-            call.respond(HttpStatusCode.OK)
-        } describe {
-            summary = "root"
-        }
-
         controlRoutes()
     }
 }
