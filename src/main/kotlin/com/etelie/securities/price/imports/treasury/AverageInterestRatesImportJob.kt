e7d11c4570cbdd34034fd579b0f615583441345d
Zachary Siegel <zachary@etelie.com>
2023-06-22 21:13:10 -0500

    standardJobDefinition

--- src/main/kotlin/com/etelie/securities/price/imports/treasury/AverageInterestRatesImportJob.kt
+++ src/main/kotlin/com/etelie/securities/price/imports/treasury/AverageInterestRatesImportJob.kt
@@ -1,13 +1,11 @@
 package com.etelie.securities.price.imports.treasury
 
 import com.etelie.schedule.Scheduler
-import com.etelie.schedule.createStandardJobDetail
-import com.etelie.schedule.createStandardTrigger
+import com.etelie.schedule.createStandardJobDefinition
 import com.etelie.schedule.logged
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.quartz.Job
 import org.quartz.JobExecutionContext
-import org.quartz.SimpleScheduleBuilder
 
 private val log = KotlinLogging.logger {}
 
@@ -17,12 +15,7 @@ class AverageInterestRatesImportJob : Job {
         private const val CRON_EXPRESSION = "0 0 12 * * *"
 
         init {
-//            Scheduler.subscribe(AverageInterestRatesImportJob::class.createStandardQuartzComponents(CRON_EXPRESSION))
-            val job = AverageInterestRatesImportJob::class.createStandardJobDetail()
-            val trigger = AverageInterestRatesImportJob::class.createStandardTrigger(
-                SimpleScheduleBuilder.simpleSchedule().repeatForever().withIntervalInSeconds(3)
-            )
-            Scheduler.subscribe(job, trigger)
+            Scheduler.subscribe(AverageInterestRatesImportJob::class.createStandardJobDefinition(CRON_EXPRESSION))
         }
     }
 
