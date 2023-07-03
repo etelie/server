88166d6aafa86c2ac5d1bc87cdabeeb71f7d6f25
Zachary Siegel <zachary@etelie.com>
2023-07-02 23:23:01 -0500

    security_price term columns

---
+++ src/main/resources/db/migration/V6__security_price_week_month_terms.sql
@@ -0,0 +1,13 @@
+alter table security_price
+  alter column term drop not null;
+
+alter table security_price
+  rename column term to term_months;
+
+alter table security_price
+  add column term_weeks integer;
+
+alter table security_price
+  add constraint constraint_term_one_nonnull check (num_nonnulls(term_weeks, term_months) = 1);
+
+comment on column security_price.term_weeks is 'unit: weeks';
