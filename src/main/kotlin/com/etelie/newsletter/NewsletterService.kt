9f919ce984bc1925865cdfb022f14316a3630b45
Zachary Siegel <zachary@etelie.com>
2023-08-16 15:19:41 -0500

    success on duplicate key

--- src/main/kotlin/com/etelie/newsletter/NewsletterService.kt
+++ src/main/kotlin/com/etelie/newsletter/NewsletterService.kt
@@ -15,12 +15,18 @@ object NewsletterService {
         ).also {
             try {
                 NewsletterTargetTable.insert(it)
+                log.info { "Inserted newsletter target :: $it" }
             } catch (e: ExposedSQLException) {
-                // todo: duplicate insert is a success
+                // Silence the exception if duplicate email address
+                if (e.message?.contains("duplicate key") == true) {
+                    return it
+                }
+
+                // todo: check key duplication before insertion - exposed logs error internally
+
                 log.error(e) { "Failed to insert newsletter target :: $it" }
                 throw EtelieException(e)
             }
-            log.info { "Inserted newsletter target :: $it" }
         }
     }
 
