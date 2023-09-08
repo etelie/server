995780b8b60d04c8bf5dfe33aacff83c561f08b7
Zachary Siegel <zachary@etelie.com>
2023-09-07 20:42:23 -0500

    Minor job management update (#45)
    * application.yaml -> application.yml
    
    * quartz log level INFO -> WARN
    
    * back to *.yaml
    
    * EtelieJob

--- src/main/kotlin/com/etelie/imports/treasury/AuctionedImportJob.kt
+++ src/main/kotlin/com/etelie/imports/treasury/AuctionedImportJob.kt
@@ -1,23 +1,20 @@
 package com.etelie.imports.treasury
 
 import com.etelie.imports.ImporterTable
+import com.etelie.schedule.EtelieJob
 import com.etelie.schedule.JobDefinition
 import com.etelie.schedule.createStandardJobDefinition
-import com.etelie.schedule.jobCoroutineContext
-import com.etelie.schedule.logged
+import io.github.oshai.kotlinlogging.KLogger
 import io.github.oshai.kotlinlogging.KotlinLogging
-import kotlinx.coroutines.runBlocking
-import org.quartz.Job
-import org.quartz.JobExecutionContext
 
 private val log = KotlinLogging.logger {}
 
-class AuctionedImportJob : Job {
+class AuctionedImportJob : EtelieJob {
 
-    override fun execute(context: JobExecutionContext?): Unit = runBlocking(jobCoroutineContext) {
-        logged(log, context) {
-            AuctionedImporter.import()
-        }
+    override val logger: KLogger = log
+
+    override suspend fun execute() {
+        AuctionedImporter.import()
     }
 
     companion object {
